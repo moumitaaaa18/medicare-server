e2b5b8a701e65cd6fc2bc2346a062ed3366f626a
moumitaaaa18 <dharchowdhurymoumita812@gmail.com>
2026-06-18 02:50:01 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vrddblm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("MongoDB Connected Successfully");
+    const database = client.db("medicareDB");
+const doctorsCollection = database.collection("doctors");
+app.get("/doctors", async (req, res) => {
+  const result = await doctorsCollection.find().toArray();
+  res.send(result);
+});
+
+    app.get("/", (req, res) => {
+      res.send("Medicare Connect Server is Running");
+    });
+
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
